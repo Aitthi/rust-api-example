54d939a9718b0a03c6d881dcaeb0edb119f67429
Aitthi Arsa <meanstack20@gmail.com>
2023-10-12 00:23:32 +0700

    🎨 api prefix

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -7,18 +7,22 @@ async fn main() {
     // address to listen on
     let addr = "0.0.0.0:1337";
     let routes = Router::new()
-        .merge(products::routes::Routes::new().get_routes())
-        .merge(users::routes::Routes::new().get_routes())
-        .merge(SwaggerUi::new("/apidoc")
-            .urls(vec![(
+        .nest(
+            "/api",
+            Router::new()
+                .merge(products::routes::Routes::new().get_routes())
+                .merge(users::routes::Routes::new().get_routes()),
+        )
+        .merge(SwaggerUi::new("/apidoc").urls(vec![
+            (
                 "/apidoc/products/openapi.json".into(),
                 products::api_doc::ApiDoc::openapi(),
             ),
             (
                 "/apidoc/users/openapi.json".into(),
                 users::api_doc::ApiDoc::openapi(),
-            )])
-        );
+            ),
+        ]));
 
     println!("\n🚀 Listening on http://{}\n", addr);
     if let Err(e) = axum::Server::bind(&addr.parse().unwrap())
