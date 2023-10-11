de133610dcf76a9798c2aacfa4951d1e5724746e
Aitthi Arsa <meanstack20@gmail.com>
2023-10-12 01:17:05 +0700

    🎨 feat BASE_PATH

--- api/products/src/api_doc.rs
+++ api/products/src/api_doc.rs
@@ -1,7 +1,14 @@
-use utoipa::OpenApi;
+use utoipa::{
+    OpenApi,
+    Modify
+};
 
 // internal
 use crate::resource;
+
+// const
+pub const BASE_PATH: &str = "/api/products";
+pub const BASE_DESCRIPTION: &str = "Base path for products API";
 
 #[derive(OpenApi)]
 #[openapi(
@@ -13,6 +20,7 @@ use crate::resource;
     paths(
         resource::create
     ),
+    modifiers(&ServerBase),
     components(
         schemas(
             resource::CreateResponse,
@@ -21,3 +29,21 @@ use crate::resource;
     )
 )]
 pub struct ApiDoc;
+
+struct ServerBase;
+impl Modify for ServerBase {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        let mut binding = Box::new(&mut openapi.paths);
+        let mut tag = utoipa::openapi::Tag::default();
+        tag.name = BASE_PATH.to_string();
+        tag.description = Some(BASE_DESCRIPTION.to_string());
+
+        openapi.tags = Some(vec![tag]);
+        for (_, item) in binding.as_mut().paths.iter_mut() {
+            item.servers = Some(vec![
+                utoipa::openapi::Server::new(BASE_PATH)
+            ]);
+        }
+
+    }
+}
--- api/users/src/api_doc.rs
+++ api/users/src/api_doc.rs
@@ -1,7 +1,11 @@
-use utoipa::OpenApi;
+use utoipa::{OpenApi, Modify};
 
 // internal
 use crate::resource;
+
+// const
+pub const BASE_PATH: &str = "/api/users";
+pub const BASE_DESCRIPTION: &str = "Base path for users API";
 
 #[derive(OpenApi)]
 #[openapi(
@@ -13,6 +17,7 @@ use crate::resource;
     paths(
         resource::login
     ),
+    modifiers(&ServerBase),
     components(
         schemas(
             resource::LoginResponse,
@@ -21,3 +26,21 @@ use crate::resource;
     )
 )]
 pub struct ApiDoc;
+
+struct ServerBase;
+impl Modify for ServerBase {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        let mut binding = Box::new(&mut openapi.paths);
+        let mut tag = utoipa::openapi::Tag::default();
+        tag.name = BASE_PATH.to_string();
+        tag.description = Some(BASE_DESCRIPTION.to_string());
+
+        openapi.tags = Some(vec![tag]);
+        for (_, item) in binding.as_mut().paths.iter_mut() {
+            item.servers = Some(vec![
+                utoipa::openapi::Server::new(BASE_PATH)
+            ]);
+        }
+
+    }
+}
