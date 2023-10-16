007d3d122578aba169f618c6f30a01036217e79a
Aitthi Arsa <meanstack20@gmail.com>
2023-10-16 23:36:42 +0700

    ✨ feat: jwt,config,database

---
+++ package/config/src/models.rs
@@ -0,0 +1,33 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Config {
+    pub runtime: Runtime,
+    pub jwt: Jwt,
+    pub database: Database,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Runtime {
+    pub addr: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Jwt {
+    pub public: String,
+    pub private: String,
+    pub expire: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum DatabaseEngine {
+    Speedb,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Database {
+    pub engine: DatabaseEngine,
+    pub file: String,
+    pub namespace: String,
+    pub database: String,
+}
